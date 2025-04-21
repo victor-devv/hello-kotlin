14ec60e54b25c595a0512c5d2d08cf42e687a6fb
Victor Ikuomola <vheecthurikuomola@gmail.com>
2025-04-21 13:15:29 +0100

    chore: arithmetic

---
+++ src/main/kotlin/Arithmetic.kt
@@ -0,0 +1,8 @@
+
+fun main() {
+    val x = 12
+    val y = 9
+    val areBothEven = x % 2 == 0 && y % 2 == 0
+    println("Is x an even number? ${x % 2 == 0}")
+    print("Are both even? $areBothEven")
+}
